eef60a878bed36cb7ef0101f989cb511a9256e8b
pedomo32 <perdomomorada@uvm.edu.ve>
2026-05-26 23:23:56 -0400

    Commit 3: Creación de entidades en memoria en data/db.js

---
+++ data/db.js
@@ -0,0 +1,10 @@
+class Database {
+    constructor() {
+        this.peliculas = [];
+        this.salas = [];
+        this.funciones = [];
+        this.tickets = [];
+        this.reservaciones = [];
+    }
+}
+module.exports = new Database();
