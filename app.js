cea5faac864c84b5c3d8180f0da0102f34c2d6cd
pedomo32 <pedomo32@example.com>
2026-06-22 23:36:56 -0400

    feat: inicializar configuracion base y conexion a base de datos mysql

--- app.js
+++ app.js
@@ -1,44 +1,25 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const methodOverride = require('method-override');
+const app = express();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var apiCinesRouter = require('./routes/apicines');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method')); 
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/api', apiCinesRouter);
+const peliculasRoutes = require('./routes/peliculas');
+const salasRoutes = require('./routes/salas');
+const reservacionesRoutes = require('./routes/reservaciones');
+const funcionesRoutes = require('./routes/funciones');
+app.use('/peliculas', peliculasRoutes);
+app.use('/salas', salasRoutes);
+app.use('/reservaciones', reservacionesRoutes);
+app.use('/funciones', funcionesRoutes);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get('/', (req, res) => {
+    res.send('<h1> Sistema del Cine</h1><p>Navega a: <a href="/peliculas">Películas</a> | <a href="/salas">Salas</a> | <a href="/reservaciones">Reservaciones</a> | <a href="/funciones">Funciones</a></p>');
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.listen(3000, () => {
+    console.log('Servidor corriendo en http://localhost:3000');
 });
-
-module.exports = app;
